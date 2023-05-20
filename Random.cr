19bb4b70a6a27f0d8e5cea730c4c88a538457cd5
hacj <jack@DESKTOP-FSB6KJ1>
2023-05-20 12:48:49 -0700

    delete old random lib

--- Random.cr
+++
@@ -1,9 +0,0 @@
-class HommRandom
-  def initialize(seed)
-    @random = Random.new(seed, sequence = 0_u64)
-  end
-  # random between to integers exclusive
-  def rint(lo : Int32, hi : Int32)
-    return @random.rand(lo..hi-1)
-  end
-end
